9d68dc0f8b9f5617cce517c200f054fed3f00d24
rndviktor2devman <7irb291wqkmu@mail.ru>
2016-11-28 20:31:58 +0000

    attempt to remove get_quote

--- static/js/get_quote.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function()
-  {
-      var data = JSON.parse($("#quotes_json").html());
-      var randomData = data[Math.floor(Math.random() * data.length)];
-      $("h1#phrase").html(randomData.phrase).show();
-      $("p#signature").html(randomData.signature).show();
-})
